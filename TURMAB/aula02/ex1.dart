0e731f4628ebf813aa92be14e89cb6512755bcf0
danielvieira95 <engdanielvieira95@gmail.com>
2025-02-03 19:13:52 -0300

    aula02- ex1.dart

---
+++ TURMAB/aula02/ex1.dart
@@ -0,0 +1,10 @@
+ import "dart:io";
+void main()
+{
+int idade = 28;
+ print("Idade: $idade");
+ double raio = 10.25;
+print("Raio: $raio");
+String nome = "Daniel";
+print("Ola $nome, seja bem vindo");
+}
