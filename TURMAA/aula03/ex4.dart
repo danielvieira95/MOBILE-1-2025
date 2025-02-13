9054867490edbd33b017149a28fcd9747ec568a2
danielvieira95 <engdanielvieira95@gmail.com>
2025-02-13 20:22:25 -0300

    aula 03
    estrutura de repetiçao

---
+++ TURMAA/aula03/ex4.dart
@@ -0,0 +1,18 @@
+/*
+Estrutura de repetiçao while
+13.02.2025
+*/
+
+void main(){
+  int cont = 0,res=0;
+
+
+  while(cont<11){
+    res = 7*cont;
+    print("Tabuada do 7");
+    print("Res = 7 * $cont = $res");
+    cont++;
+    
+
+  }
+}
