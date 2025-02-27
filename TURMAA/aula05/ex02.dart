0ad3e8cf4952ad426a785f4d47a8f2b59cd6e688
danielvieira95 <engdanielvieira95@gmail.com>
2025-02-27 19:49:18 -0300

    aula 05
    exemplos classe e objeto

---
+++ TURMAA/aula05/ex02.dart
@@ -0,0 +1,53 @@
+/*
+Exemplo 2 - Classe e objeto
+27.02.2025
+*/
+
+class Carro{
+  String marca ="Nissan" ;
+  int ano = 2025;
+
+  // cria metodo abrir 
+
+  void abrirporta(int qtde_p){
+    if(qtde_p==1){
+      print("Porta do motorista aberta");
+    }
+    else if(qtde_p==2){
+      print("Porta do motorista e do passageiro aberta");
+    }
+    else if(qtde_p==3){
+      print("Porta do motorista, do passageiro aberta e traseira direita aberta");
+    }
+    else{
+      print("Todas as portas do carro aberta");
+    }
+
+  }
+
+  void fecharporta(int qtde_p){
+    if(qtde_p==1){
+      print("Porta do motorista fechada");
+    }
+    else if(qtde_p==2){
+      print("Porta do motorista e do passageiro fechada");
+    }
+    else if(qtde_p==3){
+      print("Porta do motorista, do passageiro fechada e traseira direita fechada");
+    }
+    else{
+      print("Todas as portas do carro fechada");
+    }
+
+  }
+  void ligar(){
+    print("Carro ligado");
+  }
+
+  void desligar(){
+    print("Carro desligado");
+  }
+}
+void main(){
+  //
+}
