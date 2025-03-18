fbda5e41967ae5594b83909ccdb98489adf098b4
danielvieira95 <engdanielvieira95@gmail.com>
2025-03-17 21:51:33 -0300

    app aula 07 , exemplo b
    widgets com scaffold

---
+++ TURMAB/aula07/app_aula07b/android/app/src/main/kotlin/com/exampleapptb/app_aula07b/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.exampleapptb.app_aula07b
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ TURMAB/aula07/app_aula07b/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TelaHome());
+}
+
+class TelaHome extends StatelessWidget {
+  const TelaHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+appBar: AppBar(
+  title: Text("App aula 07",style: TextStyle(color: Colors.white),),
+  // backgroundColor - cor de fundo do app bar
+  backgroundColor: Colors.black,
+),
+body: Center(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+   
+    children: [
+        Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(width: 100,height: 100,color: Colors.black,),
+          Container(width: 100,height: 100,color: Colors.white,),
+          Container(width: 100,height: 100,color: Colors.black,),
+        ],
+      ),
+       Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(width: 100,height: 100,color: Colors.black,),
+          Container(width: 100,height: 100,color: Colors.white,),
+          Container(width: 100,height: 100,color: Colors.black,),
+        ],
+      ),
+      Text("Desenvolvimento Mobile",style: TextStyle(fontSize: 30),),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(width: 100,height: 100,color: Colors.black,),
+          Container(width: 100,height: 100,color: Colors.white,),
+          Container(width: 100,height: 100,color: Colors.black,),
+        ],
+      ),
+      ElevatedButton(onPressed: (){
+        print("Desenvolvimento Mobile");
+
+      }, child: Text("Mensagem")),
+  
+    ],
+  ),
+),
+      ),
+    );
+  }
+}
